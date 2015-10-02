8b6aa720434d00c75eec26001158184549422ccb
David Rosales <drosales007@gmail.com>
2015-10-01 20:01:50 -0500

    Fix input handler

--- HW2/Server.java
+++ HW2/Server.java
@@ -14,16 +14,15 @@ public class Server {
 
 
         // Create an array to keep track of all server ips
-        int[][] servers = new int[numServer][2];
+        String[][] servers = new String[numServer][2];
         sc.nextLine();
 
         for (int i = 0; i < numServer; i++) {
             // TODO: parse inputs to get the ips and ports of servers
             String server = sc.nextLine();
             String[] servInfo = server.split(":");
-            System.out.println(servInfo[0] + " " + servInfo[1]);
-            servers[i][0] = Integer.parseInt(servInfo[0]);
-            servers[i][1] = Integer.parseInt(servInfo[1]);
+            servers[i][0] = servInfo[0];
+            servers[i][1] = servInfo[1];
             /*System.out.println("Server IP: " + servers[i][0] + "\nServer " +
                 "Port: " + servers[i][1] + "\n\n");*/
         }

14cf65f7c66677e553b007772ea6d4756d03bf28
David Rosales <drosales007@gmail.com>
2015-10-01 21:06:21 -0500

    Finish skeleton

--- HW2/Server.java
+++ HW2/Server.java
@@ -2,7 +2,40 @@ import java.util.Scanner;
 
 public class Server {
 
-    public static void SyncWithServers(){
+    int myID;
+    String seating;
+
+    public static void SendAck(){
+        // Send an acknowledgement when a request comes in
+    }
+
+    public static void ReleaseCS(){
+        // Broadcast a message releaseing the critical section
+    }
+
+    public static void SendRequest(){
+        // Send a request to access the critical section
+    }
+
+    public static void SyncSeating(){
+        // Synchronize the reservation list with the other servers
+    }
+
+    public String Search(String[] seats, String name){
+        // Handle search requests
+    }
+
+    public String Delete(String[] seats, String name){
+        // Handle delete requests
+    }
+
+    public synchronized String Reserve(String[] seats, String name){
+        // Handle reserve requests
+    }
+
+    public synchronized String BookSeat(String[] seats, String name,
+                                        String seatNum){
+        // Handle bookSeat requests
     }
 
     public static void main (String[] args) {
@@ -20,12 +53,51 @@ public class Server {
         for (int i = 0; i < numServer; i++) {
             String server = sc.nextLine();
             String[] servInfo = server.split(":");
-            servers[i][0] = servInfo[0];
-            servers[i][1] = servInfo[1];
+            servers[i+1][0] = servInfo[0];
+            servers[i+1][1] = servInfo[1];
             /*System.out.println("Server IP: " + servers[i][0] + "\nServer " +
                 "Port: " + servers[i][1] + "\n\n");*/
         }
 
-        // TODO: handle request from clients
+        // Synchronize the reservation list on startup
+        this.seating = new String[numSeat];
+        SyncSeating()
+
+        // Handle requests from clients
+        try{
+            while (true){
+                String[] data;
+                ServerSocket srv = new ServerSocket(servers[this.myID])
+                Socket sock = new srv.accept();
+                PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+                out.println("Server Connected");
+                BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+                while (!in.ready()){
+                    // wait
+                }
+                msg = in.readLine();
+                data = msg.split(" ");
+                if (data[0].equals("RequestCS")){
+                    // Handle request
+                } else if (data[0].equals("reserve")){
+                    // Send appropriate response to client
+                } else if (data[0].equals("bookSeat")) {
+                    // Send appropriate response to client
+                } else if (data[0].equals("search")) {
+                    // Send appropriate response to client
+                } else if (data[0].equals("delete")) {
+                    // Send appropriate response to client
+                } else {
+                    System.out.println("ERROR: No such command");
+                }
+                // Send the response and close the connection
+                out.println(returnData);
+                out.close();
+                sock.close();
+                srv.close();
+            } catch(Exception e) {
+                System.out.print("Error handling the request\n");
+            }
+        }
     }
 }
